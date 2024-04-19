05a9f7055015ef68090fbe7c524d7d2606cdf818
Jasper Tan <jaspertan@ECE-A52003.austin.utexas.edu>
2024-04-19 01:26:29 -0500

    running stable diffusion

--- stable_diffusion.py
+++ stable_diffusion.py
@@ -1,15 +1,23 @@
 from diffusers import StableDiffusionPipeline
 import torch
 from cv2 import cv2
+import numpy as np
+from PIL import Image
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4")
 
+# Move the model to the device
+pipe.to(device)
+
 # Define the input
-text = "A photo of a cat."
+text = "A green colored rabbit."
 
 # Run the model
 image = pipe(text).images[0]
 
-# display the image
-cv2.imshow("image", image)
+# Display the image
+image.save("test.jpg")
+
+
